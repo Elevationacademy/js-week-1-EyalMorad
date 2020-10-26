edccf6fb75a6810ab71ba8dd8b30e5fa757dfac1
EyalMorad <72889776+EyalMorad@users.noreply.github.com>
2020-10-26 16:54:01 +0200

    Add files via upload

---
+++ Variables-And-Operators/Exercise1/main.js
@@ -0,0 +1,14 @@
+// Exercise 1
+
+const maxCapacity=14
+console.log(maxCapacity)
+
+let surfTime=true
+console.log(surfTime)
+
+let bestStudent
+console.log(bestStudent)
+
+const purposeInLife
+console.log(purposeInLife)
+
