1242ea99df7c547d3e82c66f91ab2864b42903f1
EyalMorad <72889776+EyalMorad@users.noreply.github.com>
2020-10-26 17:22:48 +0200

    Add files via upload

---
+++ Variables-And-Operators/Exercise3/main.js
@@ -0,0 +1,9 @@
+//Exercise 3
+
+const pssword = 123
+const confirmPassword = 321
+
+console.log(123>321) //print flase
+console.log(123<=321) //print true
+console.log(123==321) //print false
+console.log(123===321) //print flase
